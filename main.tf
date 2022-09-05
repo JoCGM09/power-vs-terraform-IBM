767a3641a86a24611564e7c75a3af3206f3d2bbd
JOSE CARLOS GUERRA MARTINEZ <jose.guerra.m@ibm.com>
2022-09-04 20:11:43 -0500

    main, variables & providers

---
+++ main.tf
@@ -0,0 +1,74 @@
+resource "ibm_pi_key" "power_sshkey" {
+  pi_key_name          = var.ssh_key_name
+  pi_ssh_key           = var.ssh_key
+  pi_cloud_instance_id = local.cloud_instance_id
+}
+data "ibm_pi_key" "key" {
+  depends_on           = [ibm_pi_key.power_sshkey]
+  pi_cloud_instance_id = local.cloud_instance_id
+  pi_key_name          = var.ssh_key_name
+}
+
+data "ibm_pi_image" "power_image" {
+  pi_cloud_instance_id = local.cloud_instance_id
+  pi_image_name        = var.image_name
+}
+
+resource "ibm_pi_network" "power_pub_network" {
+  count                = 1
+  pi_network_name      = "pub-network"  
+  pi_cloud_instance_id = local.cloud_instance_id
+  pi_network_type      = "pub-vlan"
+  pi_dns               = ["9.9.9.9"]
+#  pi_cidr              = "192.168.129.0/29"
+#  pi_gateway           = "192.168.0.1"
+#  pi_ipaddress_range {
+#    pi_starting_ip_address  = "192.168.0.2"
+#    pi_ending_ip_address    = "192.168.0.6"
+#  }
+}
+
+resource "ibm_pi_network" "power_priv_network" {
+  count                = 1
+  pi_network_name      = "priv-network" 
+  pi_cloud_instance_id = local.cloud_instance_id
+  pi_network_type      = "vlan"
+  pi_dns               = ["127.0.0.1"]
+  pi_cidr              = "192.168.0.0/24"
+  pi_gateway           = "192.168.0.1"
+  pi_ipaddress_range {
+    pi_starting_ip_address  = "192.168.0.2"
+    pi_ending_ip_address    = "192.168.0.254"
+  }
+}
+
+data "ibm_pi_network" "pub-network" {
+  depends_on           = [ibm_pi_network.power_pub_network]
+  pi_cloud_instance_id = local.cloud_instance_id
+  pi_network_name = "pub-network"
+}  
+  
+data "ibm_pi_network" "priv-network" {
+  depends_on           = [ibm_pi_network.power_priv_network]
+  pi_cloud_instance_id = local.cloud_instance_id
+  pi_network_name = "priv-network"
+}
+
+
+resource "ibm_pi_instance" "instance" {
+  pi_cloud_instance_id = local.cloud_instance_id
+  pi_memory            = var.memory
+  pi_processors        = var.processors
+  pi_instance_name     = var.instance_name
+  pi_proc_type         = var.processor_type
+  pi_image_id          = data.ibm_pi_image.power_image.id
+  pi_key_pair_name     = data.ibm_pi_key.key.id
+  pi_sys_type          = var.sys_type
+  pi_storage_type      = var.storage_type
+  pi_network {
+    network_id = data.ibm_pi_network.pub-network.id  
+  }
+  pi_network {
+    network_id = data.ibm_pi_network.priv-network.id  
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,58 @@
+variable "ibm_cloud_api_key" {
+  description = "API Key to use"
+  default     = "DoIJHPqCGkvt2_jcwiAf6TV7sPdLBPILmrFyfX4Bu2AX"
+}
+variable "crn" {
+  type        = string
+  default     = "crn:v1:bluemix:public:power-iaas:us-south:a/035c4a9a8dfd4a969cd9409ae37d139c:d79a6170-22cc-47a0-bede-5f141d44f6c0::"
+  description = "Power Systems Virtual Server CRN"
+}
+variable "image_name" {
+  description = "Name of the image you want to use"
+  default     = "jcgm-terraform"
+}
+variable "memory" {
+  type        = number
+  default     = 5
+  description = "The amount of memory that you want to assign to your instance in gigabytes"
+}
+variable "processors" {
+  type        = number
+  default     = 0.25
+  description = "The number of vCPUs to assign to the VM as visible within the guest Operating System"
+}
+variable "instance_name" {
+  type        = string
+  default     = "jcgm"
+  description = "The name to assign to the instance"
+}
+variable "processor_type" {
+  type        = string
+  default     = "shared"
+  description = "The type of processor mode in which the VM will run with 'shared', 'capped' or 'dedicated'"
+}
+variable "sys_type" {
+  type        = string
+  default     = "s922"
+  description = "The type of system on which to create the VM: 's922', 'e880', 'e980', 'e1080', or 's1022'"
+}
+variable "storage_type" {
+  type        = string
+  default     = "tier3"
+  description = "The type of storage tier to assign for storage volume performance: 'tier1' or 'tier3'"
+}
+variable "ssh_key_name" {
+  type        = string
+  default = "jcgmkey"
+  description = "The name of the public SSH RSA key to use when creating the instance, as defined for the selected Power Systems Virtual Server CRN"
+}
+variable "ssh_key" {
+  type        = string
+  default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQC9bojoLwVSFNhhuke0w7X19KRkXtP+xIreivtSSjKJvfxlr0h51AM7cXCpNHebexFNK658lid4g+8wySk7BxR7PFS0XrX721j4EutluM17vRxKl6PwM28AlM+aDVqKCrlLc+B2bKLZSjyVr4ktXKn9tEYwNyWTfsqQoOTIelVOZo23aaq6KIYf+KebGPZw1v5XcNP97sGIeCW8J/q0bfoF9pwiXHzS2AMFCfM6xPgFwjUoomfJ8NqbndO0jnSOH4v000jOESnGMEe5JVWYW3AsIgps6ASEjE6Z49pDkxkmAFPzIv5ro5/Olby62xv9A3RfIjEjB8ilk3RfIjEjB8ilks0hde13wretQ87gYKVnJlpAT5QrrWxJ+6jcbVD8Eyop/2KgOkyUl8ONay+PawQbX5Mqa90+ePhDmN9HndoDR3zbp52kCLiX+n7FnviT7HXlINYWxNCnJsGF+cQxnviT7HXlINYW+nShLS7BPe/dz69uNhvZMkm4TJ2Acp3FugkgQsuj28C0TTIw1wzqJ+oyWlfdg4TbAPGHND6oBAKagqHQWkHepTjTh3gKxb93ztOENAOYSJOgtCQ2fjQdTS6etzaTh3gKxb93ztOqazkH0ZYfpm3Rp0YO7RZjs7DnTylo6IA39JUN9iwQ+ek0fj9GY0OhdL3HAjCRYzR7BsUWevc53Nose.guerra.m3ogku+z9KWc8ifCaYE0IzzWNPPmgljuQ== jose.guerra.m@ibm.com"
+  description = "The name of the public SSH RSA key to use when creating the instance, as defined for the selected Power Systems Virtual Server CRN"
+}
+variable "network_name" {
+  type        = string
+  default = "jcgm-sn1"
+  description = "The network ID or name to assign to the instance, as defined for the selected Power Systems Virtual Server CRN"
+}
